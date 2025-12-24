b05ba39d2449ef2533da8425a1f6fcb53e4e624b
Gokulapriyan-51 <gokulapriyan1072001@gmail.com>
2025-12-24 15:20:02 +0530

    nextconfig updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracing: false, // 🔥 fixes EPERM on Windows + OneDrive
-  optimizeFonts: false,
-  env: {
-    emailjsPublicKey: 'JqXosOh_cPbQzyU-f',
-    emailjsServiceId: 'service_kifd2sn',
-    emailjsTemplateId: 'template_7dt6fo7'
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
